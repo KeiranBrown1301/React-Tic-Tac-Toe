30994b1c00880a62a9b03ffb66038bfe740fa5d9
Keiran Brown <keiranbrown1301@gmail.com>
2022-06-16 16:05:45 +0900

    added game component and rendered to root

--- src/index.js
+++ src/index.js
@@ -42,3 +42,22 @@ class Board extends React.Component {
     );
   }
 }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* Status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />)
